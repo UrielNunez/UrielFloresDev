1730aef5b7169a1bf521ad8c343272ebdd52db33
UrielNunez <Devuriel1@gmail.com>
2024-04-19 10:12:29 -0600

    change structure

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -108,25 +108,30 @@ document.querySelectorAll('[data-title]').forEach(element => {
     // Asigna el valor modificado al atributo data-title
     element.setAttribute('data-title', title);
 });
-/*
+
 //Evento OnClick() para acceder desde mi Project List a algun otro html
 // Obtenemos el elemento con el ID 'project1'
 const projectBoxLogIn = document.getElementById('project1');
 // Agregamos un evento de clic al elemento
 projectBoxLogIn.addEventListener('click', function() {
     // Redireccionamos al archivo index.html deseado
-    window.location.replace ('./Login/SignUp.html');
+    window.location.replace ('./SignUp.html');
     
-});*/
+});
 
 
-
+/*
 const projectBoxLogIn = document.getElementById('project1');
 projectBoxLogIn.addEventListener('click', function() {
-    fetch('../Login/SignUp.html')
+    fetch('./Login.html')
     .then(response => response.text())
     .then(html => {
         document.body.innerHTML = html;
+        // Vincular un archivo CSS externo
+        const linkTag = document.createElement('link');
+        linkTag.rel = 'stylesheet';
+        linkTag.href = './Assets/CSS/Stylelogin.css'; // Reemplaza con la ruta correcta
+        document.head.appendChild(linkTag);
     })
     .catch(error => console.error('Error al cargar el HTML:', error));
-});
+});*/
